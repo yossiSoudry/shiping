9accd03e68d56bbd1bef1245e6161f4d84a74117
yossiSoudry <yosisudri@gmail.com>
2024-02-29 18:53:36 +0200

    0.5

--- app/(layout)/Navbar.tsx
+++ app/(layout)/Navbar.tsx
@@ -7,7 +7,7 @@ import {
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Image } from "@nextui-org/react";
+import { Image, cn } from "@nextui-org/react";
 import {
   BadgeInfo,
   Contact2,
@@ -42,48 +42,76 @@ const Navbar = () => {
             <SheetTrigger>
               <MenuIcon className="w-8 h-8 text-blue-900" />
             </SheetTrigger>
-            <SheetContent>
+            <SheetContent className="min-h-screen flex flex-col justify-between">
               <div className="space-y-2 py-4 flex flex-col">
                 {menuItems.map((item, id) => (
                   <SheetClose key={id}>
                     <div
                       id={item.title}
-                      className={`flex items-center gap-4 py-2 text-base font-semibold leading-7 text-blue-800 tap-highlight-transparent`}
+                      className={`flex items-center gap-4 py-2 text-base font-semibold leading-7 text-blue-900/90 tap-highlight-transparent`}
                       onClick={() => route.push(item.href)}
                     >
-                      {item.icon}
+                      <div className="text-orange-400/90">{item.icon}</div>
                       {item.title}
                     </div>
                   </SheetClose>
                 ))}
+              </div>
+              <div className="flex flex-col gap-4">
+                <Link href="https://members.lionwheel.com/locate/locate_task?org=%D7%A9%D7%99%D7%A4%D7%99%D7%A0%D7%92%20%D7%9E%D7%A9%D7%9C%D7%95%D7%97%D7%99%D7%9D">
+                  <Button
+                    color="primary"
+                    variant="outline"
+                    className="w-full bg-orange-400/90 text-white"
+                  >
+                    בירור משלוח
+                  </Button>
+                </Link>
+                <Link href="https://members.lionwheel.com/?locale=he">
+                  <Button
+                    color="primary"
+                    variant="outline"
+                    className="w-full bg-blue-900/90 text-white"
+                  >
+                    כניסת משתמשים
+                  </Button>
+                </Link>
               </div>
             </SheetContent>
           </Sheet>
         </div>
         <div className="hidden lg:flex gap-4">
           <Link href="https://members.lionwheel.com/locate/locate_task?org=%D7%A9%D7%99%D7%A4%D7%99%D7%A0%D7%92%20%D7%9E%D7%A9%D7%9C%D7%95%D7%97%D7%99%D7%9D">
-            <Button color="primary" variant="outline">
+            <Button
+              color="primary"
+              variant="outline"
+              className="bg-orange-400/90 text-white"
+            >
               בירור משלוח
             </Button>
           </Link>
           <Link href="https://members.lionwheel.com/?locale=he">
-            <Button color="primary" variant="outline">
+            <Button
+              color="primary"
+              variant="outline"
+              className="bg-blue-900/90 text-white"
+            >
               כניסת משתמשים
             </Button>
           </Link>
         </div>
-
         <div className="gap-4 hidden lg:flex">
           {menuItems.map((item, id) => (
             <Link
               key={id}
               href={item.href}
               aria-current="page"
-              className={`whitespace-nowrap text-blue-800 hover:opacity-70 active:text-orange-500 ${
+              className={cn(
+                "whitespace-nowrap text-blue-900/90 hover:opacity-70 active:text-orange-400 duration-300",
                 pathname == item.href
-                  ? "text-orange-500 font-semibold"
+                  ? "text-orange-400 font-semibold"
                   : "dark:text-white"
-              } duration-300`}
+              )}
             >
               {item.title}
             </Link>
