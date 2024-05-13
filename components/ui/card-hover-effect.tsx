2f70e5e6d6ac417c5f3e3c739c3525b7f2723835
yossiSoudry <yosisudri@gmail.com>
2024-05-14 02:57:49 +0300

    more accessibility

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -100,7 +100,7 @@ export const CardDescription = ({
   return (
     <p
       className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        "mt-8 text-zinc-500 tracking-wide leading-relaxed text-sm",
         className
       )}
     >
