e31d341c37b1bde6bca75d9441a7417f97ce6cc1
YossiSoudry <yosisudri@gmail.com>
2024-08-19 22:23:12 +0300

    0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,24 +20,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
-      className={
-        cn(
-        inter.className, 
-        // "relative min-h-screen"
-        ) 
-        }
-        >
+      <body
+        className={cn(
+          inter.className
+          // "relative min-h-screen"
+        )}
+      >
         {/* <Providers> */}
-          <Navbar />
-          <div className="h-screen pt-20 relative">
-            {children}
-            </div>
-            <a href="https://wa.me/972527600768" className="fixed left-4 bottom-4 z-50" aria-label="פתח צ'אט תמיכה">
-            <MdOutlineWhatsapp className="w-12 h-12 text-green-500 bg-white rounded-full" />
-            </a>
+        <Navbar />
+        <div className="h-screen pt-20 relative">{children}</div>
+        <a
+          href="https://wa.me/972527600768?text=היי,%20הגעתי%20דרך%20האתר%20שלכם.%20תוכלו%20לעזור%20לי%20בבקשה%20עם%20העניין%20הבא:"
+          className="fixed left-4 bottom-4 z-50"
+          aria-label="פתח צ'אט תמיכה"
+        >
+          <MdOutlineWhatsapp className="w-12 h-12 text-green-500 bg-white rounded-full" />
+        </a>
         {/* </Providers> */}
-        <script defer src="https://cdn.enable.co.il/licenses/enable-L260270heypcosni-0524-59023/init.js"></script>
+        <script
+          defer
+          src="https://cdn.enable.co.il/licenses/enable-L260270heypcosni-0524-59023/init.js"
+        ></script>
       </body>
     </html>
   );
