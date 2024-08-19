e7941e524038b42d59dc252e81f7f435a3f39340
YossiSoudry <yosisudri@gmail.com>
2024-08-19 22:12:46 +0300

    עדכון טלפון תמיכה

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           <div className="h-screen pt-20 relative">
             {children}
             </div>
-            <a href="https://wa.me/972539231086" className="fixed left-4 bottom-4" aria-label="פתח צ'אט תמיכה">
+            <a href="https://wa.me/972527600768" className="fixed left-4 bottom-4" aria-label="פתח צ'אט תמיכה">
             <MdOutlineWhatsapp className="w-12 h-12 text-green-500 bg-white rounded-full" />
             </a>
         {/* </Providers> */}
