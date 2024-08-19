2156946e14e90aa2f91749e3de90dc3d65b82ba9
YossiSoudry <yosisudri@gmail.com>
2024-08-19 22:31:02 +0300

    0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <Navbar />
         <div className="h-screen pt-20 relative">{children}</div>
         <a
-          href="https://wa.me/972527600768?text=היי,%20הגעתי%20דרך%20האתר%20שלכם.%20תוכלו%20לעזור%20לי%20בבקשה%20עם%20העניין%20הבא:"
+          href="https://wa.me/972527600768?text=היי,%20הגעתי%20דרך%20האתר%20שלכם.%0Aאשמח%20אם%20תוכלו%20לעזור%20לי%20בבקשה%20עם%20העניין%20הבא:"
           className="fixed left-4 bottom-4 z-50"
           aria-label="פתח צ'אט תמיכה"
         >
